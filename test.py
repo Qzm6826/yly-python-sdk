c95e9ea7b9bd0f110eaad040825143a2dff7e3d2
lwy <1139038165@qq.com>
2019-01-26 14:07:12 +0800

    修改：     .gitignore 	修改：     .travis.yml 	新文件：   test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from Lib.Config.config import Config
+from Lib.Oauth.oauth import Oauth
+from Lib.Protocol.rpc_client import RpcClient
+from Lib.Api.yly_print import YlyPrint
+print('Hello World')
