85c12f268761dc69df588beab47fcb3c7d4a77e2
lwy <1139038165@qq.com>
2019-01-26 11:06:11 +0800

    修改：     .gitignore 	修改：     setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 setup(
     name="yly-python-sdk",
-    version="1.0",
+    version="1.1.0",
     keywords=("python", "openapi"),
     description="yly openapi python sdk",
     license="MIT Licence",
